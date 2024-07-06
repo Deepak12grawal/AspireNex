2434def984e2614f5443e1a70701751c715ab298
Deepak agrawal <agrawaldeepak017@gmail.com>
2024-07-06 13:25:55 +0530

    Done with Updated UI!!

---
+++ personal_portfolio/src/component/About.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I am Deepak, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in CSE, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education
+        </h1>
+        <span>
+          Bachelor degree,Computer Science an Engineering,C.T.A.E Udaipur, 2022-2026  
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in C++ language, Experienced with Version Control System, Strong grasp of Data Structure and Algorithim & Excellent problem-solving skills Effective communicator and collaborator
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          Web Development internship, CodSoft, 10 jun-10 july Major Achievement is to enhance mu skills with 3 project
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative in web developer solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
