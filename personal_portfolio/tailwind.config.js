03a3bd498cdf09d1f543ce4b0ee15e9fdfc3189f
deepak <agrawaldeepak017@gmail.com>
2025-08-25 17:31:25 +0530

    Resolve conflicts

--- personal_portfolio/tailwind.config.js
+++ personal_portfolio/tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
   content: [ 
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",  
   ],
-=======
-  content: ["./src/**/*.{html,js}"],
->>>>>>> 080843e8ee0b52cea63e84b18960f3d0e1204498
   theme: {
     extend: {},
   },
